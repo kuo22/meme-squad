fb85366177b5891ea992e404fde1d42a331ecb18
Yulong Tan <yulongtan67@gmail.com>
2018-03-11 15:24:54 -0700

    change points to onclick

--- scripts/gridmap.js
+++ scripts/gridmap.js
@@ -125,11 +125,11 @@ map.on('load', function() {
     }
   });
 
-  map.on('mousemove', 'tweets-point', function(e) {
+  map.on('click', 'tweets-point', function(e) {
   new mapboxgl.Popup()
     .setLngLat(e.features[0].geometry.coordinates)
     .setHTML('<b>Name:</b> ' + e.features[0].properties.Name + 
-             '<em>@</em>' + e.features[0].properties.Handle + '<br>' +
+             ' <i>@' + e.features[0].properties.Handle + '</i> <br>' +
              '<b>Bio:</b> ' + e.features[0].properties.Bio + '<br>' +
              '<b>Location:</b> ' + e.features[0].properties.Place)
     .addTo(map);
