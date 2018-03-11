d61a9cfc51bd33dcd6058cda303fa96fc7635cee
Yulong Tan <yulongtan67@gmail.com>
2018-03-11 03:39:24 -0700

    add interactivity

--- scripts/gridmap.js
+++ scripts/gridmap.js
@@ -83,8 +83,56 @@ map.on('load', function() {
     },
   }
 }, 'waterway-label');
-  // add heatmap layer here
-  // add circle layer here
+
+  map.addLayer({
+    id: 'tweets-point',
+    type: 'circle',
+    source: 'tweets',
+    minzoom: 14,
+    paint: {
+      // increase the radius of the circle as the zoom level and dbh value increases
+      'circle-radius': {
+        property: 'dbh',
+        type: 'exponential',
+        stops: [
+          [{ zoom: 15, value: 1 }, 5],
+          [{ zoom: 15, value: 62 }, 10],
+          [{ zoom: 22, value: 1 }, 20],
+          [{ zoom: 22, value: 62 }, 50],
+        ]
+      },
+      'circle-color': {
+        property: 'dbh',
+        type: 'exponential',
+        stops: [
+          [0, 'rgba(236,222,239,0)'],
+          [10, 'rgb(236,222,239)'],
+          [20, 'rgb(208,209,230)'],
+          [30, 'rgb(166,189,219)'],
+          [40, 'rgb(103,169,207)'],
+          [50, 'rgb(28,144,153)'],
+          [60, 'rgb(1,108,89)']
+        ]
+      },
+      'circle-stroke-color': 'white',
+      'circle-stroke-width': 1,
+      'circle-opacity': {
+        stops: [
+          [14, 0],
+          [15, 1]
+        ]
+      }
+    }
+  }, 'waterway-label');
+
+  map.on('mousemove', 'tweets-point', function(e) {
+  new mapboxgl.Popup()
+    .setLngLat(e.features[0].geometry.coordinates)
+    .setHTML('<b>DBH:</b> ' + e.features[0].properties.dbh)
+    .addTo(map);
+});
+
+
 
 });
 
