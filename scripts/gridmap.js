87e68ac91c8b0f9203a602e5bb6ba0f359933505
Yulong Tan <yulongtan67@gmail.com>
2018-03-11 03:32:44 -0700

    add geojson for tweets

--- scripts/gridmap.js
+++ scripts/gridmap.js
@@ -19,7 +19,7 @@ map.on('load', function() {
     $key.addClass('legend-key');
     $key.css({
       backgroundColor: color
-    })
+    });
 
     var $value = $(document.createElement('span'));
     $value.html(interval);
@@ -27,6 +27,65 @@ map.on('load', function() {
     $value.appendTo($item);
     $item.appendTo(legend);
   }
+
+  map.addSource('tweets', {
+    type: 'geojson',
+    data: 'data/tweets.geojson'
+  });
+
+  map.addLayer({
+  id: 'tweets-heat',
+  type: 'heatmap',
+  source: 'tweets',
+  maxzoom: 15,
+  paint: {
+    // increase weight as diameter breast height increases
+    'heatmap-weight': {
+      property: 'dbh',
+      type: 'exponential',
+      stops: [
+        [1, 0],
+        [62, 1]
+      ]
+    },
+    // increase intensity as zoom level increases
+    'heatmap-intensity': {
+      stops: [
+        [11, 1],
+        [15, 3]
+      ]
+    },
+    // assign color values be applied to points depending on their density
+    'heatmap-color': [
+      'interpolate',
+      ['linear'],
+      ['heatmap-density'],
+      0, 'rgba(236,222,239,0)',
+      0.2, 'rgb(208,209,230)',
+      0.4, 'rgb(166,189,219)',
+      0.6, 'rgb(103,169,207)',
+      0.8, 'rgb(28,144,153)'
+    ],
+    // increase radius as zoom increases
+    'heatmap-radius': {
+      stops: [
+        [11, 15],
+        [15, 20]
+      ]
+    },
+    // decrease opacity to transition into the circle layer
+    'heatmap-opacity': {
+      default: 1,
+      stops: [
+        [14, 1],
+        [15, 0]
+      ]
+    },
+  }
+}, 'waterway-label');
+  // add heatmap layer here
+  // add circle layer here
+
 });
 
 map.on('mousemove', function(e) {
